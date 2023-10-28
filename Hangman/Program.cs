684eeb326872cafb6a91c894ecfb2a28e8459a63
Raymond Hart <z3nithvip3r@gmail.com>
2023-10-28 11:38:54 -0500

    Formatted code

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -28,50 +28,50 @@
             {
                 displayedWord[i] = '_';
             }
-                int incorrectTries = 0;
-                while (incorrectTries < MAX_ATTEMPTS)
+            int incorrectTries = 0;
+            while (incorrectTries < MAX_ATTEMPTS)
+            {
+                Console.WriteLine("Hangman Game");
+                Console.WriteLine("============");
+                Console.WriteLine($"Guess the letters to a {mysteryWord.Length} letter word.\n");
+                Console.WriteLine(displayedWord);
+                Console.WriteLine();
+
+                Console.WriteLine("Enter a letter:");
+                char upperKeyChar = char.ToUpper(Console.ReadKey().KeyChar);
+                Console.Clear(); //Clear text
+                Console.WriteLine($"You entered {upperKeyChar}\n");
+
+                bool isCorrect = false;
+
+                for (int i = 0; i < mysteryWord.Length; i++)
                 {
-                    Console.WriteLine("Hangman Game");
-                    Console.WriteLine("============");
-                    Console.WriteLine($"Guess the letters to a {mysteryWord.Length} letter word.\n");
-                    Console.WriteLine(displayedWord);
-                    Console.WriteLine();
-
-                    Console.WriteLine("Enter a letter:");
-                    char upperKeyChar = char.ToUpper(Console.ReadKey().KeyChar);
-                    Console.Clear(); //Clear text
-                    Console.WriteLine($"You entered {upperKeyChar}\n");
-
-                    bool isCorrect = false;
-                    
-                    for (int i = 0; i < mysteryWord.Length; i++)
+                    if (mysteryWord[i] == upperKeyChar)
                     {
-                        if (mysteryWord[i] == upperKeyChar)
-                        {
-                            isCorrect = true;
-                            displayedWord[i] = upperKeyChar;
-                        }
-                    }
-
-                    if (isCorrect == true)
-                    {
-                        Console.WriteLine("Correct! You have " + (MAX_ATTEMPTS - incorrectTries) + " attempts remaining.\n");
-                    }
-                    else
-                    {
-                        incorrectTries++;
-                        Console.WriteLine("Wrong! You have " + (MAX_ATTEMPTS - incorrectTries) + " attempts remaining.\n");
-                    }
-
-                    if (new string(displayedWord) == mysteryWord)
-                    {
-                        hasUserWon |= true;
-                        break;
+                        isCorrect = true;
+                        displayedWord[i] = upperKeyChar;
                     }
                 }
+
+                if (isCorrect == true)
+                {
+                    Console.WriteLine("Correct! You have " + (MAX_ATTEMPTS - incorrectTries) + " attempts remaining.\n");
+                }
+                else
+                {
+                    incorrectTries++;
+                    Console.WriteLine("Wrong! You have " + (MAX_ATTEMPTS - incorrectTries) + " attempts remaining.\n");
+                }
+
+                if (new string(displayedWord) == mysteryWord)
+                {
+                    hasUserWon |= true;
+                    break;
+                }
+            }
             if (hasUserWon == true)
-            { 
-                Console.WriteLine($"Congratulations you guessed the correct word! It was {mysteryWord}\n"); 
+            {
+                Console.WriteLine($"Congratulations you guessed the correct word! It was {mysteryWord}\n");
             }
             else
             {
